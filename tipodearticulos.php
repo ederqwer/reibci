087ea5dde564ed00fde00b7169961f3cebc13e4d
Luis <a2133330018@gmail.com>
2018-07-17 02:48:07 -0500

    ok

---
+++ tipodearticulos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles/css/footer.css">
+</head>
+<body>
+    <div  style="padding-top:6em;"class="container">
+        
+        <?php $option = 5;
+        include("templates/navbartop.php"); ?>
+        <div style=" background-color: white;" class="jumbotron">    
+        <br><br><br>
+
+            <p  style="text-align:center;font-size: 2.5em;">
+            Tipos de artículos
+            </p > 
+            
+            <p style="border: 0.5px solid black;"></p> 
+
+            <p style="text-align:justify;font-size: 1.25em;margin-top:2em;">
+                Publicamos artículos relacionados con la investigación en cualquier área de la ciencia, respetando los siguientes formatos:</p>
+                <ul>
+                    <li>De investigación originales. Aquellos que describen la hipótesis, el estudio y los métodos de la investigación. Reporta resultados, su interpretación y puede incluir la discusión de las posibles implicaciones. </li>
+                    <li>De reflexión. Documento que presenta resultados de investigación en proceso o terminada, en el cual se desarrolla una perspectiva analítica, interpretativa o crítica del autor, sobre un tema específico, recurriendo a fuentes originales. </li>
+                    <li>De revisión. Resultado de una investigación en proceso o terminada en el cual se analizan, sistematizan e integran los avances y/o resultados de investigaciones publicadas o no publicadas, la intención es dar cuenta de sus tendencias y desarrollo. </li>
+                    <li>Artículos cortos. Documentos breves que presentan resultados originales preliminares o parciales de una investigación científica o tecnológica, que por lo general requieren de una pronta difusión. </li>
+                    <li>Reportes de casos. Textos que dan cuenta de los resultados de un estudio sobre una situación particular, con el fin de dar a conocer las experiencias técnicas y metodológicas consideradas en un caso específico. Debe incluir una revisión sistemática comentada sobre casos similares. </li>
+                    <li>Síntesis o revisión de temas. Documentos de revisión crítica, que cubren tópicos que han tenido un desarrollo significativo o progreso en los últimos años, de forma comprehensiva, perspectiva balanceada, profundidad intelectual y que sean de amplio interés general. No son meras recapitulaciones de la literatura. </li>
+                    <li>De reflexión. Que presenten resultados de investigaciones realizadas con una intención analítica, interpretativa o crítica del autor, sobre una temática determinada, en la que se haya recurrido a fuentes originales. </li>
+                    <li>Informes tecnológicos. Proporcionan información sobre nuevos métodos o técnicas emergentes introducen los fundamentos técnicos básicos del método, evalúan las ventajas, las limitaciones del enfoque y de sus aplicaciones .</li> 
+                    <li>De opinión. Permiten a los investigadores publicar puntos de vista sobre la interpretación de los hechos , el valor de los métodos utilizados, las debilidades y los puntos fuertes de cualquier teoría científica o sobre cualquier tema relevante para cualquier campo de investigación . Admite la máxima libertad de expresión para los investigadores, con la intención de fomentar el debate constructivo. Deben estar respaldados por pruebas (ya sea de hecho o anecdótica ) y promover el diálogo. </li>
+                </ul>
+
+
+        </div>
+        <?php include("templates/footer.php"); ?>
+    </div>
+</body>
+</html>
